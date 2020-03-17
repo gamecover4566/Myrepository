e3d214c07543fc5530ab2ef1dd02f20f68594d50
gamecover4566 <hanju45666@gmail.com>
2020-03-17 18:33:30 +0900

    spring ajax 예제 연습1

---
+++ springAjax/src/main/java/com/itwill/ajax/controller/CustomMappingJackson2JsonView.java
@@ -0,0 +1,26 @@
+package com.itwill.ajax.controller;
+
+import java.util.Collection;
+import java.util.Map;
+
+import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
+
+public class CustomMappingJackson2JsonView extends MappingJackson2JsonView {
+	@Override
+	protected Object filterModel(Map<String, Object> model) {
+		
+		Object result = super.filterModel(model);
+		if(!(result instanceof Map)) {
+			return result;
+		}
+		
+		Map resultMap = (Map)result;
+		if(resultMap.size() == 1) {
+			Collection c = resultMap.values();
+			Object[] oa = c.toArray();
+			return oa[0];
+		}
+		
+		return super.filterModel(model);
+	}
+}
